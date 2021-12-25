77e7a98f9b313983d5bcc8a3e7c8cee336b8d831
samsunga50 <samsunga50.in.2020@gmail.com>
2021-12-25 20:54:59 +0400

    better template

--- lib/createRecipe.dart
+++ lib/createRecipe.dart
@@ -5,7 +5,29 @@ class MyCustomForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("hello")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter a search term',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Enter your username',
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
